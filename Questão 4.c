fa30023a4c3a9d43a7586c2b4090175617cbc4aa
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-02-29 18:35:42 -0300

    Add files via upload

---
+++ Questão 4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+
+	//Declarando variáveis
+	 int num1, antecessor, sucessor;
+	 
+	//Obtendo dados
+	printf("Digite um número: ");
+		scanf("%i",&num1);
+		
+	antecessor = num1-1;
+	sucessor = num1+1;
+	
+	
+	//Exibindo resultado
+	printf("\n============= R E S U L T A D O ============");
+	printf("\n\nNúmero digitado: %d",num1);
+	printf("\nNúmero Antecessor: %d",antecessor);
+	printf("\nNúmero Sucessor: %d",sucessor);
+	
+	
+	return 0 ;
+
+}
