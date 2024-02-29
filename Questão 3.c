2e518085c5359f01306278b86dbbafb38fe22ff1
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-02-29 17:51:27 -0300

    Add files via upload

---
+++ Questão 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	//Declarando variáveis 
+	
+	int numero1;
+	
+	//Obtendo dados do usuário 
+	
+	printf("Digite um Número: ");
+		scanf("%i",&numero1);
+		
+	if(numero1% == 0){
+		printf("\t\t\tPar");
+	} else{
+		printf("\t\t\tImpar");
+	}
+	
+}
