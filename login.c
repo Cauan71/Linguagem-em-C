1d8f2f774bd70a3e31158b140b93aa35292aaac1
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-03-01 16:02:30 -0300

    Add files via upload

---
+++ login.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+	setlocale(LC_ALL,"");
+	
+	char login[200], senha[200];
+	char loginCadastrado[200] = "Usuario";
+	char senhaCadastrada[200] = "123";
+	
+	
+	
+	printf("Digite o seu Login: ");
+		scanf("%s",&login);
+		
+	printf("Digite a sua senha: ");
+		scanf("%s",&senha);
+		
+		//&& == e
+		// || == ou
+		
+		//Verificando se o login e senha estão corretos.
+		//if(login == loginCadastrado && senha == senhaCadastrada){
+		if(strcmp(login, loginCadastrado) == 0 && strcmp(senha, senhaCadastrada) == 0 ){
+			printf("Bem Vindo!!");
+		}else{
+			printf("Acesso Negado");
+			
+		}
+		
+		return 0 ;
+		
+		
+
+}
