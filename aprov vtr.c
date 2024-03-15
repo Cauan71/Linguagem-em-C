c6652b65b11ba3ca7838d90fba6875d785a15008
Cauan71 <126925374+Cauan71@users.noreply.github.com>
2024-03-15 14:20:20 -0300

    Add files via upload

---
+++ aprov vtr.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>	
+#include <stdlib.h>
+
+//#define TAM 4
+
+	int main(){
+		setlocale(LC_ALL,"");
+		
+		//float notas[TAM]
+		float notas[4], soma=0, media;
+		int i;
+		char resultado[200];
+		
+		
+			printf("\n===Solicitanto notas =====\n");
+			
+		//for(i = 0 ; i < TAM  ; i++){
+		for(i = 0 ; i < 4  ; i++){
+			do{
+			
+			printf("Digite a %i nota : ", i+1);
+				scanf("%f",&notas[i]);
+	
+			
+			
+		}while(notas[i]<0 || notas[i]>10);
+	
+		soma += notas[i];
+		}
+			//media = soma/ TAM;
+			media = soma / i;
+			
+		if(media>=7){
+			strcpy(resultado, "APROVADO!!");
+			
+		}if(media<7 && media >=5){
+			strcpy(resultado,"RECUPERAÇÃO");
+			
+		}
+		if(media<5){
+			strcpy(resultado,"REPROVADO");
+		
+		}
+			
+		system("cls || clear");
+	
+		printf("\n======= R E S U L T A D O =======\n");
+		
+		for(i = 0; i < 4; i++){
+			
+			printf("%iº nota: %.1f	\n", i+1 , notas[i]);
+		}
+		
+		printf("Média : %.1f \n",media);
+		printf("Situação: %s",resultado);
+		
+	
+	
+	return 0;
+	
+}
+	
